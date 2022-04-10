1eb9737206854a50c9c449210017208ed06081b5
Inflexible <ilikcay24@gmail.com>
2022-04-10 09:12:25 +0200

    work 1

---
+++ src/us100/TestCase_001.java
@@ -0,0 +1,12 @@
+package us100;
+
+public class TestCase_001 {
+
+    // omer> omer2
+
+    public static void main(String[] args) {
+
+
+        System.out.println("OMERCIM IS KIND OF BORING");
+    }
+}
